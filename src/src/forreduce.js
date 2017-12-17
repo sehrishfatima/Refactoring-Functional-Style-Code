333198922a19e3693367e336b223d62c5ffef828
Angeelina <rajkarnikarangeelina@gmail.com>
2017-12-17 15:13:53 +0100

    for loop

--- src/src/forreduce.js
+++ src/src/forreduce.js
@@ -1,6 +1,7 @@
 var animals = ["cat","dog","fish"];
 var total = 0;
 var item;
+//for loop
 for (var count = 0, loops = animals.length; count < loops; count++){
     item = animals[count];
     total += item.length;
