fc8a1e91b3a35659aa99703237aeede5fd5ce63b
sehrish <sehrish.fatima12@gmail.com>
2017-12-17 15:22:35 +0100

    test
    testcode

--- src/src/forfilter.js
+++ src/src/forfilter.js
@@ -1,6 +1,7 @@
 var words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
 
 //result = 0;
+//test
 for(var i = 0; i < words.length; i++){
     if (words[i].length > 6){
         const result = words[i];
